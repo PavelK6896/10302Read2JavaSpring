80709645ddc147878804d0ed89898b21bff49274
PavelK6896 <79208776896@yandex.ru>
2022-06-11 15:52:21 +0300

    add native 2 docker file

---
+++ read2.native2.Dockerfile
@@ -0,0 +1,61 @@
+ARG deployBuild="http://localhost:8080"
+
+FROM ghcr.io/graalvm/native-image:ol8-java17 AS builder
+RUN microdnf install maven
+
+RUN mvn -v
+RUN native-image --version
+
+FROM builder AS verify
+ADD --chown=mvn:mvn pom.xml /home/app/pom.xml
+WORKDIR /home/app
+RUN mvn verify -P native -D skipTests -D maven.test.skip=true -e --fail-never
+
+FROM node:16.15.1-alpine AS build-front
+RUN apk add curl unzip zip
+
+ARG token="-"
+ARG appNameFolderGit="10317Read2TSAngular-master"
+
+RUN curl -H "Authorization: token $token" -L -O https://github.com/PavelK6896/10317Read2TSAngular/archive/refs/heads/master.zip
+RUN unzip -d ./source/ master.zip
+
+WORKDIR /source/$appNameFolderGit/
+RUN npm install
+RUN npm run "build prod static"
+
+ARG deployBuild
+ARG appName="10317-read2-ts-angular"
+ARG search="http://localhost:8080"
+ARG files="./dist/$appName/*"
+
+RUN for f in $files; do \
+         if [[ $search != "" && $deployBuild != "" && "$f" == *"main"* ]]; then \
+           sed -i "s+$search+$deployBuild+g" "$f"; \
+         fi \
+       done
+
+WORKDIR /
+RUN cp -r ./source/10317Read2TSAngular-master/dist/10317-read2-ts-angular/ mian
+
+FROM verify AS maven
+ADD --chown=mvn:mvn /src /home/app/src
+
+RUN rm -r /home/app/src/main/resources/static/main
+COPY --from=build-front "/mian" /home/app/src/main/resources/static/main
+
+WORKDIR /home/app
+
+RUN mvn clean package -P native -D skipTests -D maven.test.skip=true
+
+FROM oraclelinux:8-slim
+ARG deployBuild
+ENV NAME_APP=read2-app
+ENV HOST=$deployBuild
+
+COPY --from=maven "/home/app/target/$NAME_APP" spring-boot-native
+CMD [ "sh", "-c", "./spring-boot-native " ]
+
+
+# docker build -t read2-v2 -f read2.native2.Dockerfile .
+# docker run --name read2-v2-c1 -p 8080:8080 -d read2-v2
--- src/main/java/app/web/pavelk/read2/controller/StaticController.java
+++ src/main/java/app/web/pavelk/read2/controller/StaticController.java
@@ -2,48 +2,13 @@ package app.web.pavelk.read2.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.stream.Stream;
 
 @Slf4j(topic = "static-controller")
 @Controller
 @RequiredArgsConstructor
 public class StaticController {
-
-    @Value("${app.host:}")
-    private String host;
-
-    @PostConstruct
-    public void setBaseUrlStaticFile() throws URISyntaxException, IOException {
-        URL resource = Thread.currentThread().getContextClassLoader().getResource("static/main/");
-        if (resource != null) {
-            Path path = Paths.get(resource.toURI());
-            try (Stream<Path> walk = Files.walk(path)) {
-                walk.filter(Files::isRegularFile)
-                        .filter(f -> f.getFileName().toString().contains("main"))
-                        .forEach(f -> {
-                            try {
-                                String replace = Files.readString(f).replaceFirst("https://localhost:8081", host);
-                                Files.write(f, replace.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.SYNC, StandardOpenOption.TRUNCATE_EXISTING);
-                                log.info("{}/api/read2/", host);
-                            } catch (IOException e) {
-                                throw new IllegalArgumentException(e);
-                            }
-                        });
-            }
-        }
-    }
 
     @GetMapping("/")
     public String main() {
