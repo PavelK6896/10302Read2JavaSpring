a88966709510999a8e63f82844ecb4f7eb262a16
PavelK6896 <79208776896@yandex.ru>
2022-06-23 19:28:11 +0300

    update security config

--- src/main/java/app/web/pavelk/read2/config/SecurityConfig.java
+++ src/main/java/app/web/pavelk/read2/config/SecurityConfig.java
@@ -3,27 +3,27 @@ package app.web.pavelk.read2.config;
 
 import app.web.pavelk.read2.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     public static final String[] open_path_get = {"/sub-read/**", "/post/**", "/comment/**", "/main/**", "/", "/property"};
     public static final String[] swagger_path = {"/v3/api-docs/**", "/swagger-ui/**"};
     public static final String[] open_path = {"/auth/**"};
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, open_path_get).permitAll()
@@ -35,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-    }
 
+        return httpSecurity.build();
+    }
 }
